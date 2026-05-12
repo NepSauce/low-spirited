95fc7401c07751cb02eb192eafd87b5ee7b99b84
NepSauce <zawadatif.builds@gmail.com>
2026-05-12 18:23:59 -0300

    Feat: Implement LowSpiritedModel with embedding layer

---
+++ model/low_spirited.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+class LowSpiritedModel(nn.Module):
+    def __init__(self, vocab_size, embed_dim):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_dim)
+    
+    def forward(self, idx):
+        x = self.embedding(idx)
+
+        return x
