7b88c04b7454d451156568d4e99be23b1bc5952c
NepSauce <zawadatif.builds@gmail.com>
2026-05-13 18:26:02 -0300

    Feat: Add attention projection layers

--- model/low_spirited.py
+++ model/low_spirited.py
@@ -1,12 +1,16 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class LowSpiritedModel(nn.Module):
     def __init__(self, vocab_size, embed_dim):
         super().__init__()
 
         self.embedding = nn.Embedding(vocab_size, embed_dim)
-        self.lm_head == nn.Linear(embed_dim, vocab_size)
+        self.key = nn.Linear(embed_dim, embed_dim, bias=False)
+        self.query = nn.Linear(embed_dim, embed_dim, bias=False)
+        self.value = nn.Linear(embed_dim, embed_dim, bias=False)
+        self.lm_head = nn.Linear(embed_dim, vocab_size)
     
     def forward(self, idx, targets=None):
         x = self.embedding(idx)
