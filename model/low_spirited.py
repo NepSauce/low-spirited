a32a1aa4fdcea7af369e11308c4b317c534aee7e
NepSauce <zawadatif.builds@gmail.com>
2026-05-13 21:21:04 -0300

    Feat: Implement forward method for MultiHeadAttention class

--- model/low_spirited.py
+++ model/low_spirited.py
@@ -17,6 +17,35 @@ class MultiHeadAttention(nn.Module):
 
         self.proj = nn.Linear(embed_dim)
 
+    def forward(self, x):
+        B, T, C = x.shape
+
+        k = self.key(x)
+        q = self.query(x)
+        v = self.value(x)
+
+        k = k.view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
+        q = q.view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
+        v = v.view(B, T, self.num_heads, self.head_dim).transpose(1, 2)
+
+        # Measures similarity between tokens
+        attn = q @ k.transpose(-2, -1)
+        attn = attn / (self.head_dim ** 0.5)
+
+        mask = torch.tril(torch.ones(T, T, device=x.device))
+
+        attn = attn.masked_fill(mask == 0, float('-inf'))
+        attn = F.softmax(attn, dim=-1)
+
+        out = attn @ v
+        out = out.transpose(1, 2).contiguous().view(B, T, C)
+
+        # Final projection
+        return self.proj(out)
+
+
+
+
 class LowSpiritedModel(nn.Module):
     def __init__(self, vocab_size, embed_dim):
         super().__init__()
